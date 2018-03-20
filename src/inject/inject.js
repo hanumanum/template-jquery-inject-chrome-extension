da1340d2b6b7a2863dcbd896d996f3fcb981cb1c
artak (hanuman) kolyan <a.kolyan@gratun.am>
2018-03-20 16:34:06 +0400

    initial commit

---
+++ src/inject/inject.js
@@ -0,0 +1,17 @@
+chrome.extension.sendMessage({}, function (response) {
+	var readyStateCheckInterval = setInterval(function () {
+		if (document.readyState === "complete") {
+			clearInterval(readyStateCheckInterval);
+
+			// ----------------------------------------------------------
+			// This part of the script triggers when page is done loading
+			console.log("Hello. This message was sent from scripts/inject.js");
+			// ----------------------------------------------------------
+			jQuery(document).ready(function () {
+				//TODO: script here
+				console.log("jquery works");
+			});
+
+		}
+	}, 10);
+});
